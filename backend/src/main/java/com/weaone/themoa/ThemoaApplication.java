f3dc9b64c133b857e5783892cd9aa70f302cff91
Kimsolmin <solmin614@gmail.com>
2026-07-07 08:55:40 +0900

    commit

---
+++ backend/src/main/java/com/weaone/themoa/ThemoaApplication.java
@@ -0,0 +1,13 @@
+package com.weaone.themoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThemoaApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/weaone/themoa/ThemoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weaone.themoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
