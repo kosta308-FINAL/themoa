598e385fe5f3f5a6679da70ec4444194fd32bd1e
Kimsolmin <solmin614@gmail.com>
2026-07-18 19:08:26 +0900

    sm add test code

--- backend/src/test/java/com/weaone/themoa/domain/cardtransaction/service/CardTransactionBackfillServiceTest.java
+++ backend/src/test/java/com/weaone/themoa/domain/cardtransaction/service/CardTransactionBackfillServiceTest.java
@@ -27,6 +27,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.math.BigDecimal;
@@ -58,6 +59,8 @@ class CardTransactionBackfillServiceTest {
     private CardTransactionCollectionService cardTransactionCollectionService;
     @Mock
     private NotificationService notificationService;
+    @Mock
+    private ApplicationEventPublisher eventPublisher;
 
     @InjectMocks
     private CardTransactionBackfillService cardTransactionBackfillService;
